4fbd000486e2247f712f84630096467a93e1a2ef
Andrey <scorpion.and@ukr.net>
2022-10-30 17:10:50 +0200

    Complete task "Set up Passport"

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 
@@ -11,6 +13,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 app.route('/').get((req, res) => {
   res.render(
@@ -18,6 +26,9 @@ app.route('/').get((req, res) => {
     { title: 'Hello', message: 'Please login' }
   );
 });
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
